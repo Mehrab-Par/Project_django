7bdbe324a9e87f4c55fb4c7b612a6051b89741e7
Mehrab <mehrab.par1314@gamil.com>
2026-09-20 22:16:37 -0700

    part #1

---
+++ mysite/pages/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [	
+    path("hello/", views.HomePageView.as_view(), name="home"), 
+    path("about/",views.AboutPageView.as_view(),name="about"),  
+]
