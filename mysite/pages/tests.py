5bf0f25213f5aff505e0d9d7ad86eb45c99e4037
Mehrab <mehrab.par1314@gamil.com>
2026-09-22 01:32:39 -0700

    part #2

--- mysite/pages/tests.py
+++ mysite/pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase , SimpleTestCase
 
 # Create your tests here.
+
+
+class SimpleTest(SimpleTestCase):
+    def test_home_psage_status_code(self):
+        response = self.client.get('/hello/')
+        self.assertEqual(response.status_code,200)
+        
+    def test_about_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
