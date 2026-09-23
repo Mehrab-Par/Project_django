14828d08a2f1120bd2ffb980446203299c3e085e
Mehrab <mehrab.par1314@gamil.com>
2026-09-23 00:26:13 -0700

    part #3

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import ListView
 # Create your views here.
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     context = {
+#         "posts": posts,
+#     }
+#     return render(request, "posts/list.html",context)
+
+class PostListView(ListView):
+    model= Post
+    template_name= "posts/list.html"
